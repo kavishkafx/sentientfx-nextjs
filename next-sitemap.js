c84385297cc6e4010464465099b2fdbf8d293be9
kavishka <kavishkadahanayaka@gmail.com>
2024-11-21 23:17:35 +0530

    add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: 'https://sentientfx.online', // Replace with your website URL
+  generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+  changefreq: 'daily', // (Optional) Frequency of content change
+  priority: 0.7, // (Optional) Priority of pages
+  sitemapSize: 7000, // (Optional) Maximum URLs per sitemap file
+};
