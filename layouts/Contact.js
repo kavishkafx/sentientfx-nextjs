e999996adf69b433e31d7c23b4e88f08ab0809e1
kavishka <kavishkadahanayaka@gmail.com>
2024-11-23 00:31:10 +0530

    .....

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,12 +1,64 @@
-import ContactForm from '/layouts/components/ContactForm';
+import config from "@config/config";
+import { markdownify } from "@lib/utils/textConverter";
 
-const About = () => {
+const Contact = ({ data }) => {
+  const { frontmatter } = data;
+  const { title } = frontmatter;
+  const { contact_form_action } = config.params;
+
   return (
-    <div>
-      <h1>Contact Us</h1>
-      <ContactForm />
-    </div>
+    <section className="section">
+      <div className="container max-w-[700px]">
+        {markdownify(title, "h1", "h2 mb-8 text-center")}
+        <form
+          className="contact-form"
+          method="POST"
+          action={contact_form_action}
+        >
+          <div className="mb-6">
+            <label className="mb-2 block" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="form-input w-full"
+              name="name"
+              type="text"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="mb-2 block" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="form-input w-full"
+              name="email"
+              type="email"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="mb-2 block" htmlFor="subject">
+              Subject
+            </label>
+            <input
+              className="form-input w-full"
+              name="subject"
+              type="text"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="mb-2 block" htmlFor="message">
+              Message
+            </label>
+            <textarea className="form-textarea w-full" rows="7" />
+          </div>
+          <button className="btn btn-outline-primary">Submit Now</button>
+        </form>
+      </div>
+    </section>
   );
 };
 
-export default About;
+export default Contact;
