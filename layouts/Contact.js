5855aaa666e1334e1628db5780043d6bad0746f0
kavishka <kavishkadahanayaka@gmail.com>
2024-11-23 00:48:03 +0530

    test again

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,70 +1,17 @@
+import { useEffect } from 'react';
 import config from "@config/config";
 import { markdownify } from "@lib/utils/textConverter";
 
 const Contact = ({ data }) => {
   const { frontmatter } = data;
   const { title } = frontmatter;
-  const { contact_form_action } = config.params;
 
-  return (
-    <section className="section">
-      <div className="container max-w-[700px]">
-        {markdownify(title, "h1", "h2 mb-8 text-center")}
-        <form
-          className="contact-form"
-          method="POST"
-          action={contact_form_action}
-          data-netlify="true" // Add this line for Netlify form handling
-        >
-          <div className="mb-6">
-            <label className="mb-2 block" htmlFor="name">
-              Name
-            </label>
-            <input
-              className="form-input w-full"
-              name="name"
-              type="text"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="mb-2 block" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="form-input w-full"
-              name="email"
-              type="email"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="mb-2 block" htmlFor="subject">
-              Subject
-            </label>
-            <input
-              className="form-input w-full"
-              name="subject"
-              type="text"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="mb-2 block" htmlFor="message">
-              Message
-            </label>
-            <textarea 
-              className="form-textarea w-full" 
-              name="message" // Ensure you include the name attribute for the textarea
-              rows="7" 
-              required // Add this to make it a required field
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">Submit Now</button>
-        </form>
-      </div>
-    </section>
-  );
+  useEffect(() => {
+    // Redirect to form.html when the component mounts
+    window.location.href = '/form.html';
+  }, []);
+
+  return null; // No need to render anything in this component
 };
 
 export default Contact;
