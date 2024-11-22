3302912843cb41716d25a5dd4efb433476368715
kavishka <kavishkadahanayaka@gmail.com>
2024-11-22 23:01:37 +0530

    .

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -1,33 +1,20 @@
-import config from "@config/config";
-import { markdownify } from "@lib/utils/textConverter";
-
 const Contact = ({ data }) => {
   const { frontmatter } = data;
   const { title } = frontmatter;
-  const { contact_form_action } = config.params;
 
   return (
     <section className="section">
       <div className="container max-w-[700px]">
         {markdownify(title, "h1", "h2 mb-8 text-center")}
-        <form
-          className="contact-form"
-          name="contact"
-          method="POST"
-          netlify-honeypot="bot-field"
+        <form 
+          name="contact" 
+          method="POST" 
+          action="/"
           data-netlify="true"
         >
           <input type="hidden" name="form-name" value="contact" />
-          <p className="hidden">
-            <label>
-              Don't fill this out if you're human: 
-              <input name="bot-field" />
-            </label>
-          </p>
           <div className="mb-6">
-            <label className="mb-2 block" htmlFor="name">
-              Name
-            </label>
+            <label className="mb-2 block" htmlFor="name">Name</label>
             <input
               className="form-input w-full"
               name="name"
@@ -36,9 +23,7 @@ const Contact = ({ data }) => {
             />
           </div>
           <div className="mb-6">
-            <label className="mb-2 block" htmlFor="email">
-              Email
-            </label>
+            <label className="mb-2 block" htmlFor="email">Email</label>
             <input
               className="form-input w-full"
               name="email"
@@ -47,9 +32,7 @@ const Contact = ({ data }) => {
             />
           </div>
           <div className="mb-6">
-            <label className="mb-2 block" htmlFor="subject">
-              Subject
-            </label>
+            <label className="mb-2 block" htmlFor="subject">Subject</label>
             <input
               className="form-input w-full"
               name="subject"
@@ -58,9 +41,7 @@ const Contact = ({ data }) => {
             />
           </div>
           <div className="mb-6">
-            <label className="mb-2 block" htmlFor="message">
-              Message
-            </label>
+            <label className="mb-2 block" htmlFor="message">Message</label>
             <textarea 
               className="form-textarea w-full" 
               name="message" 
